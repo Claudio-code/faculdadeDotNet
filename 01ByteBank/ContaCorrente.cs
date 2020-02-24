6c7414c303f1ba2166b39d298ffe5f252ab71979
claudio-code <suporte2424@outlook.com>
2020-02-24 17:27:16 -0300

    primeira classe do c#

---
+++ 01ByteBank/ContaCorrente.cs
@@ -0,0 +1,12 @@
+namespace _01ByteBank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public int numAgencia;
+        public int numero;
+        public double saldo;
+
+        
+    }
+}
